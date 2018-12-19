9ed3cf3c2282f3c308ef771974f63a1219831b96
amnzo <salmi.ensa.ilsi@gmail.com>
2018-12-19 13:47:34 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var express = require('express'),
-app = express();
+//Install express server
+const express = require('express');
+const path = require('path');
 
-app.use(express.static(__dirname+'/dist/firetest'));
+const app = express();
 
-var server = app.listen(process.env.PORT || 8080);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/firetest'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/firetest/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
