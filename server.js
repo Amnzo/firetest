e25b6b179a250fff687f114d0590b33291c08ca3
amnzo <salmi.ensa.ilsi@gmail.com>
2018-12-19 14:59:05 +0100

    updates to deploy to heroku2

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+var express = require('express'),
+app = express();
 
-const app = express();
+app.use(express.static(__dirname+'/dist/firetest'));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/firetest'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/firetest/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+var server = app.listen(process.env.PORT || 8080);
